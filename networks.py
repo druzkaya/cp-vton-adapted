78a71fe69cd122e1b17cb1114bf7b0f51b53dc04
Aurélia Nègre <aurelianegre@MacBook-Pro-de-Aurelia.local>
2020-02-20 16:47:28 +0100

    [refactor] adapted .yml and commented all cuda code

--- networks.py
+++ networks.py
@@ -65,7 +65,7 @@ class FeatureExtraction(nn.Module):
         model += [nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1), nn.ReLU(True)]
         model += [norm_layer(512)]
         model += [nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1), nn.ReLU(True)]
-        
+
         self.model = nn.Sequential(*model)
         init_weights(self.model, init_type='normal')
 
@@ -80,11 +80,11 @@ class FeatureL2Norm(torch.nn.Module):
         epsilon = 1e-6
         norm = torch.pow(torch.sum(torch.pow(feature,2),1)+epsilon,0.5).unsqueeze(1).expand_as(feature)
         return torch.div(feature,norm)
-    
+
 class FeatureCorrelation(nn.Module):
     def __init__(self):
         super(FeatureCorrelation, self).__init__()
-    
+
     def forward(self, feature_A, feature_B):
         b,c,h,w = feature_A.size()
         # reshape features for matrix multiplication
@@ -94,9 +94,9 @@ class FeatureCorrelation(nn.Module):
         feature_mul = torch.bmm(feature_B,feature_A)
         correlation_tensor = feature_mul.view(b,h,w,h*w).transpose(2,3).transpose(1,2)
         return correlation_tensor
-    
+
 class FeatureRegression(nn.Module):
-    def __init__(self, input_nc=512,output_dim=6, use_cuda=True):
+    def __init__(self, input_nc=512,output_dim=6, use_cuda=False):
         super(FeatureRegression, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(input_nc, 512, kernel_size=4, stride=2, padding=1),
@@ -128,19 +128,19 @@ class FeatureRegression(nn.Module):
 
 class AffineGridGen(nn.Module):
     def __init__(self, out_h=256, out_w=192, out_ch = 3):
-        super(AffineGridGen, self).__init__()        
+        super(AffineGridGen, self).__init__()
         self.out_h = out_h
         self.out_w = out_w
         self.out_ch = out_ch
-        
+
     def forward(self, theta):
         theta = theta.contiguous()
         batch_size = theta.size()[0]
         out_size = torch.Size((batch_size,self.out_ch,self.out_h,self.out_w))
         return F.affine_grid(theta, out_size)
-        
+
 class TpsGridGen(nn.Module):
-    def __init__(self, out_h=256, out_w=192, use_regular_grid=True, grid_size=3, reg_factor=0, use_cuda=True):
+    def __init__(self, out_h=256, out_w=192, use_regular_grid=True, grid_size=3, reg_factor=0, use_cuda=False):
         super(TpsGridGen, self).__init__()
         self.out_h, self.out_w = out_h, out_w
         self.reg_factor = reg_factor
@@ -177,12 +177,12 @@ class TpsGridGen(nn.Module):
                 self.P_X_base = self.P_X_base.cuda()
                 self.P_Y_base = self.P_Y_base.cuda()
 
-            
+
     def forward(self, theta):
         warped_grid = self.apply_transformation(theta,torch.cat((self.grid_X,self.grid_Y),3))
-        
+
         return warped_grid
-    
+
     def compute_L_inverse(self,X,Y):
         N = X.size()[0] # num of points (along dim 0)
         # construct matrix K
@@ -193,21 +193,21 @@ class TpsGridGen(nn.Module):
         K = torch.mul(P_dist_squared,torch.log(P_dist_squared))
         # construct matrix L
         O = torch.FloatTensor(N,1).fill_(1)
-        Z = torch.FloatTensor(3,3).fill_(0)       
+        Z = torch.FloatTensor(3,3).fill_(0)
         P = torch.cat((O,X,Y),1)
         L = torch.cat((torch.cat((K,P),1),torch.cat((P.transpose(0,1),Z),1)),0)
         Li = torch.inverse(L)
         if self.use_cuda:
             Li = Li.cuda()
         return Li
-        
+
     def apply_transformation(self,theta,points):
         if theta.dim()==2:
             theta = theta.unsqueeze(2).unsqueeze(3)
         # points should be in the [B,H,W,2] format,
-        # where points[:,:,:,0] are the X coords  
-        # and points[:,:,:,1] are the Y coords  
-        
+        # where points[:,:,:,0] are the X coords
+        # and points[:,:,:,1] are the Y coords
+
         # input are the corresponding control points P_i
         batch_size = theta.size()[0]
         # split theta into point coordinates
@@ -215,16 +215,16 @@ class TpsGridGen(nn.Module):
         Q_Y=theta[:,self.N:,:,:].squeeze(3)
         Q_X = Q_X + self.P_X_base.expand_as(Q_X)
         Q_Y = Q_Y + self.P_Y_base.expand_as(Q_Y)
-        
+
         # get spatial dimensions of points
         points_b = points.size()[0]
         points_h = points.size()[1]
         points_w = points.size()[2]
-        
+
         # repeat pre-defined control points along spatial dimensions of points to be transformed
         P_X = self.P_X.expand((1,points_h,points_w,1,self.N))
         P_Y = self.P_Y.expand((1,points_h,points_w,1,self.N))
-        
+
         # compute weigths for non-linear part
         W_X = torch.bmm(self.Li[:,:self.N,:self.N].expand((batch_size,self.N,self.N)),Q_X)
         W_Y = torch.bmm(self.Li[:,:self.N,:self.N].expand((batch_size,self.N,self.N)),Q_Y)
@@ -239,12 +239,12 @@ class TpsGridGen(nn.Module):
         # A_X,A,Y: size [B,H,W,1,3]
         A_X = A_X.unsqueeze(3).unsqueeze(4).transpose(1,4).repeat(1,points_h,points_w,1,1)
         A_Y = A_Y.unsqueeze(3).unsqueeze(4).transpose(1,4).repeat(1,points_h,points_w,1,1)
-        
+
         # compute distance P_i - (grid_X,grid_Y)
         # grid is expanded in point dim 4, but not in batch dim 0, as points P_X,P_Y are fixed for all batch
         points_X_for_summation = points[:,:,:,0].unsqueeze(3).unsqueeze(4).expand(points[:,:,:,0].size()+(1,self.N))
         points_Y_for_summation = points[:,:,:,1].unsqueeze(3).unsqueeze(4).expand(points[:,:,:,1].size()+(1,self.N))
-        
+
         if points_b==1:
             delta_X = points_X_for_summation-P_X
             delta_Y = points_Y_for_summation-P_Y
@@ -252,31 +252,31 @@ class TpsGridGen(nn.Module):
             # use expanded P_X,P_Y in batch dimension
             delta_X = points_X_for_summation-P_X.expand_as(points_X_for_summation)
             delta_Y = points_Y_for_summation-P_Y.expand_as(points_Y_for_summation)
-            
+
         dist_squared = torch.pow(delta_X,2)+torch.pow(delta_Y,2)
         # U: size [1,H,W,1,N]
         dist_squared[dist_squared==0]=1 # avoid NaN in log computation
-        U = torch.mul(dist_squared,torch.log(dist_squared)) 
-        
+        U = torch.mul(dist_squared,torch.log(dist_squared))
+
         # expand grid in batch dimension if necessary
         points_X_batch = points[:,:,:,0].unsqueeze(3)
         points_Y_batch = points[:,:,:,1].unsqueeze(3)
         if points_b==1:
             points_X_batch = points_X_batch.expand((batch_size,)+points_X_batch.size()[1:])
             points_Y_batch = points_Y_batch.expand((batch_size,)+points_Y_batch.size()[1:])
-        
+
         points_X_prime = A_X[:,:,:,:,0]+ \
                        torch.mul(A_X[:,:,:,:,1],points_X_batch) + \
                        torch.mul(A_X[:,:,:,:,2],points_Y_batch) + \
                        torch.sum(torch.mul(W_X,U.expand_as(W_X)),4)
-                    
+
         points_Y_prime = A_Y[:,:,:,:,0]+ \
                        torch.mul(A_Y[:,:,:,:,1],points_X_batch) + \
                        torch.mul(A_Y[:,:,:,:,2],points_Y_batch) + \
                        torch.sum(torch.mul(W_Y,U.expand_as(W_Y)),4)
-        
+
         return torch.cat((points_X_prime,points_Y_prime),3)
-        
+
 # Defines the Unet generator.
 # |num_downs|: number of downsamplings in UNet. For example,
 # if |num_downs| == 7, image of size 128x128 will become of size 1x1
@@ -386,7 +386,7 @@ class VGGLoss(nn.Module):
     def __init__(self, layids = None):
         super(VGGLoss, self).__init__()
         self.vgg = Vgg19()
-        self.vgg.cuda()
+        #self.vgg.cuda()
         self.criterion = nn.L1Loss()
         self.weights = [1.0/32, 1.0/16, 1.0/8, 1.0/4, 1.0]
         self.layids = layids
@@ -405,13 +405,13 @@ class GMM(nn.Module):
     """
     def __init__(self, opt):
         super(GMM, self).__init__()
-        self.extractionA = FeatureExtraction(22, ngf=64, n_layers=3, norm_layer=nn.BatchNorm2d) 
+        self.extractionA = FeatureExtraction(22, ngf=64, n_layers=3, norm_layer=nn.BatchNorm2d)
         self.extractionB = FeatureExtraction(3, ngf=64, n_layers=3, norm_layer=nn.BatchNorm2d)
         self.l2norm = FeatureL2Norm()
         self.correlation = FeatureCorrelation()
-        self.regression = FeatureRegression(input_nc=192, output_dim=2*opt.grid_size**2, use_cuda=True)
-        self.gridGen = TpsGridGen(opt.fine_height, opt.fine_width, use_cuda=True, grid_size=opt.grid_size)
-        
+        self.regression = FeatureRegression(input_nc=192, output_dim=2*opt.grid_size**2, use_cuda=False)
+        self.gridGen = TpsGridGen(opt.fine_height, opt.fine_width, use_cuda=False, grid_size=opt.grid_size)
+
     def forward(self, inputA, inputB):
         featureA = self.extractionA(inputA)
         featureB = self.extractionB(inputB)
@@ -428,11 +428,10 @@ def save_checkpoint(model, save_path):
         os.makedirs(os.path.dirname(save_path))
 
     torch.save(model.cpu().state_dict(), save_path)
-    model.cuda()
+    #model.cuda()
 
 def load_checkpoint(model, checkpoint_path):
     if not os.path.exists(checkpoint_path):
         return
     model.load_state_dict(torch.load(checkpoint_path))
-    model.cuda()
-
+    #model.cuda()
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 #coding=utf-8
 import torch
 import torch.nn as nn
@@ -19,7 +20,7 @@ def get_opt():
     parser.add_argument("--gpu_ids", default = "")
     parser.add_argument('-j', '--workers', type=int, default=1)
     parser.add_argument('-b', '--batch-size', type=int, default=4)
-    
+
     parser.add_argument("--dataroot", default = "data")
     parser.add_argument("--datamode", default = "train")
     parser.add_argument("--stage", default = "GMM")
@@ -38,7 +39,7 @@ def get_opt():
     return opt
 
 def test_gmm(opt, test_loader, model, board):
-    model.cuda()
+    #model.cuda()
     model.eval()
 
     base_name = os.path.basename(opt.checkpoint)
@@ -54,41 +55,41 @@ def test_gmm(opt, test_loader, model, board):
 
     for step, inputs in enumerate(test_loader.data_loader):
         iter_start_time = time.time()
-        
+
         c_names = inputs['c_name']
-        im = inputs['image'].cuda()
-        im_pose = inputs['pose_image'].cuda()
-        im_h = inputs['head'].cuda()
-        shape = inputs['shape'].cuda()
-        agnostic = inputs['agnostic'].cuda()
-        c = inputs['cloth'].cuda()
-        cm = inputs['cloth_mask'].cuda()
-        im_c =  inputs['parse_cloth'].cuda()
-        im_g = inputs['grid_image'].cuda()
-            
+        im = inputs['image']#.cuda()
+        im_pose = inputs['pose_image']#.cuda()
+        im_h = inputs['head']#.cuda()
+        shape = inputs['shape']#.cuda()
+        agnostic = inputs['agnostic']#.cuda()
+        c = inputs['cloth']#.cuda()
+        cm = inputs['cloth_mask']#.cuda()
+        im_c =  inputs['parse_cloth']#.cuda()
+        im_g = inputs['grid_image']#.cuda()
+
         grid, theta = model(agnostic, c)
         warped_cloth = F.grid_sample(c, grid, padding_mode='border')
         warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
         warped_grid = F.grid_sample(im_g, grid, padding_mode='zeros')
 
-        visuals = [ [im_h, shape, im_pose], 
-                   [c, warped_cloth, im_c], 
+        visuals = [ [im_h, shape, im_pose],
+                   [c, warped_cloth, im_c],
                    [warped_grid, (warped_cloth+im)*0.5, im]]
-        
-        save_images(warped_cloth, c_names, warp_cloth_dir) 
-        save_images(warped_mask*2-1, c_names, warp_mask_dir) 
+
+        save_images(warped_cloth, c_names, warp_cloth_dir)
+        save_images(warped_mask*2-1, c_names, warp_mask_dir)
 
         if (step+1) % opt.display_count == 0:
             board_add_images(board, 'combine', visuals, step+1)
             t = time.time() - iter_start_time
             print('step: %8d, time: %.3f' % (step+1, t), flush=True)
-        
+
 
 
 def test_tom(opt, test_loader, model, board):
-    model.cuda()
+    #model.cuda()
     model.eval()
-    
+
     base_name = os.path.basename(opt.checkpoint)
     save_dir = os.path.join(opt.result_dir, base_name, opt.datamode)
     if not os.path.exists(save_dir):
@@ -99,28 +100,28 @@ def test_tom(opt, test_loader, model, board):
     print('Dataset size: %05d!' % (len(test_loader.dataset)), flush=True)
     for step, inputs in enumerate(test_loader.data_loader):
         iter_start_time = time.time()
-        
+
         im_names = inputs['im_name']
-        im = inputs['image'].cuda()
+        im = inputs['image']#.cuda()
         im_pose = inputs['pose_image']
         im_h = inputs['head']
         shape = inputs['shape']
 
-        agnostic = inputs['agnostic'].cuda()
-        c = inputs['cloth'].cuda()
-        cm = inputs['cloth_mask'].cuda()
-        
+        agnostic = inputs['agnostic']#.cuda()
+        c = inputs['cloth']#.cuda()
+        cm = inputs['cloth_mask']#.cuda()
+
         outputs = model(torch.cat([agnostic, c],1))
         p_rendered, m_composite = torch.split(outputs, 3,1)
         p_rendered = F.tanh(p_rendered)
         m_composite = F.sigmoid(m_composite)
         p_tryon = c * m_composite + p_rendered * (1 - m_composite)
 
-        visuals = [ [im_h, shape, im_pose], 
-                   [c, 2*cm-1, m_composite], 
+        visuals = [ [im_h, shape, im_pose],
+                   [c, 2*cm-1, m_composite],
                    [p_rendered, p_tryon, im]]
-            
-        save_images(p_tryon, im_names, try_on_dir) 
+
+        save_images(p_tryon, im_names, try_on_dir)
         if (step+1) % opt.display_count == 0:
             board_add_images(board, 'combine', visuals, step+1)
             t = time.time() - iter_start_time
@@ -131,8 +132,8 @@ def main():
     opt = get_opt()
     print(opt)
     print("Start to test stage: %s, named: %s!" % (opt.stage, opt.name))
-   
-    # create dataset 
+
+    # create dataset
     train_dataset = CPDataset(opt)
 
     # create dataloader
@@ -142,7 +143,7 @@ def main():
     if not os.path.exists(opt.tensorboard_dir):
         os.makedirs(opt.tensorboard_dir)
     board = SummaryWriter(log_dir = os.path.join(opt.tensorboard_dir, opt.name))
-   
+
     # create model & train
     if opt.stage == 'GMM':
         model = GMM(opt)
@@ -156,7 +157,7 @@ def main():
             test_tom(opt, train_loader, model, board)
     else:
         raise NotImplementedError('Model [%s] is not implemented' % opt.stage)
-  
+
     print('Finished test %s, named: %s!' % (opt.stage, opt.name))
 
 if __name__ == "__main__":
